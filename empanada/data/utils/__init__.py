b2c04a3ef54e6c22085027c11bac57626a66fc83
conradry <conradrw@nih.gov>
2022-01-17 10:31:25 -0500

    fix copy paste name

--- empanada/data/utils/__init__.py
+++ empanada/data/utils/__init__.py
@@ -1,4 +1,4 @@
 from empanada.data.utils.sampler import DistributedWeightedSampler
-from empanada.data.utils.copy_paste_class import copy_paste_class
+from empanada.data.utils.copy_paste import copy_paste_class
 from empanada.data.utils.target_creation import heatmap_and_offsets
 from empanada.data.utils.transforms import CopyPaste, FactorPad, resize_by_factor
